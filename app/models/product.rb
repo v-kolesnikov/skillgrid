96fb4b345e23f81a37ecb8d789ecce50354c3ff9
kolesnikov <re.vkolesnikov@gmail.com>
2015-09-20 18:48:19 +0300

    Add models validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
+
+  validates :name, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :products, foreign_key: :owner_id
 
+  validates :email, :login, presence: true
+  validates :login, presense: true
+  validates :password, length: { minimum: 6 }, if: :new_record?
+
   has_secure_password
 end
